867cd33b7742c9db2fef2a09a35d82e91c5da7b3
Kevin Huang <kevin@notablehealth.com>
2020-08-04 14:46:59 -0700

    update to dataset.tar.gz

--- download_dataset.sh
+++ download_dataset.sh
@@ -1,3 +1,3 @@
 mkdir dataset
-aws s3 cp s3://utech2020/expression_detector/expression.zip dataset/expression.zip
-unzip dataset/expression.zip -d dataset/
+aws s3 cp s3://utech2020/expression_detector/dataset.tar.gz dataset/dataset.tar.gz
+tar xzf dataset/dataset.tar.gz -C dataset/
